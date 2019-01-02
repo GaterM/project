ded22344ed3bbead0ba6af22d19af323ee4cf52e
GasterM <1096816096@qq.com>
2019-01-02 11:23:45 +0800

    pro

--- test/test.py
+++ test/test.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/product/', methods=['GET'])
+def product():
+    return jsonify({"p_name": '11111'})
+
+
+if __name__ == '__main__':
+    app.run()
